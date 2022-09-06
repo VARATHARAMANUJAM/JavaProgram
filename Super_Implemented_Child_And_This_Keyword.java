fa9afcabda1c7e72139d69bc00cd4c0c8d586841
ramanujam <rockramanujam2222@gmail.com>
2022-09-06 08:20:29 +0530

    This and Super keyword used to differ

---
+++ Super_Implemented_Child_And_This_Keyword.java
@@ -0,0 +1,42 @@
+package My_Basic_programs_OOPS;
+
+public class Super_Implemented_Child_And_This_Keyword extends Super_Parent {
+	
+	public static void main(String[] args) {
+		Super_Implemented_Child_And_This_Keyword obj = new Super_Implemented_Child_And_This_Keyword();
+       // obj.run();
+        //obj.color();
+		obj.eating();
+        System.out.println(obj.color);
+        
+	}
+
+	
+	String color ="black";
+	int eat = 5;
+	void eating() {
+		super.eating();
+		int eat =4;
+		int total = eat +this.eat;
+		System.out.println("eating" + total);
+	}
+	public void run() {
+		eating();
+		super.jog();
+		super.run();
+		super.eating();
+		System.out.println("run");
+	}
+	protected void jog() {
+		run();
+		super.jog();
+		System.out.println("jog");
+	}
+	
+	final void color() {
+	String color = this.color + super.color;
+		 System.out.println(color);
+		 
+	}
+	
+}
---
+++ Super_Parent.java
@@ -0,0 +1,17 @@
+package My_Basic_programs_OOPS;
+
+public class Super_Parent {
+	
+	String color ="white";
+	
+	void eating() {
+		System.out.println("eating");
+	}
+	public void run() {
+		System.out.println("run");
+	}
+	protected void jog() {
+		System.out.println("jog");
+	}
+
+}
